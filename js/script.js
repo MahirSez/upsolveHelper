269375a4ff7e93b520ce58ef6195bc66c82666f6
MahirSez <mahirsezan@hmail.com>
2020-06-05 03:09:06 +0600

    added problem lists dynamically

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@
 
 	var upHelper = {};
 	var contestListHTML = "snippets/contest-list-snippet.html";
+	var problemElementHTML = "snippets/problem-element-snippet.html";
+
+	var problemsURL = "https://codeforces.com/api/problemset.problems";
+	var ratingChangeURL = "https://codeforces.com/api/user.rating?handle=";
+
+	var limit = 50;
 
 
 	var insertHTML = function(selector, html) {
 		document.querySelector(selector).innerHTML = html;
+	}
+	var appendHTML = function(selector, html) {
+		if(document.querySelector(selector).innerHTML == false) {
+			document.querySelector(selector).innerHTML = html;
+		}
+		else document.querySelector(selector).innerHTML += html;
 	}
 
 	var insertText = function(selector, text) {
@@ -18,27 +30,59 @@
 		return string;
 	}
 
+	var loadProblems = function(response) {
+		if(response.status != "OK") return;
+
+		upHelper.problems = response.result.problems.reverse();
+		upHelper.problemStats = response.result.problemStatistics;
+
+		$ajaxUtil.sendGetRequest(problemElementHTML, function(response) {
+
+			for(var i = 0 ; i < upHelper.ratingChange.length ; i++ ) {
+
+				var ok = false;
+				for(var j = 0 ; j < upHelper.problems.length ; j++ ) {
+
+					if( upHelper.ratingChange[i]["contestId"] == upHelper.problems[j]["contestId"] ) {
+						var htmlCode = response;
+						htmlCode = insertProperty(htmlCode, "problem_name", upHelper.problems[j]["contestId"]  + upHelper.problems[j]["index"] + " - "+ upHelper.problems[j]["name"] )
+						htmlCode = insertProperty(htmlCode, "contest_id",  upHelper.problems[j]["contestId"] );
+						htmlCode = insertProperty(htmlCode, "problem_index",  upHelper.problems[j]["index"] );
+						appendHTML("#id_" + upHelper.problems[j]["contestId"], htmlCode);
+						ok = true;
+					}
+				}
+				if(ok == false) {
+					console.log("well that was unexpected!, no info about contest: " + upHelper.ratingChange[i]["contestId"] );
+				}
+			}
+		}, false);
+	}
+
 	var loadContestNames = function(response) {
 
-		console.log(upHelper.result);
-		if(upHelper.result.length > 0) {
-			insertText("h1", upHelper.result[0].handle);
+		
+		if(upHelper.ratingChange.length > 0) {
+			insertText("h1", upHelper.ratingChange[0].handle);
 		}
 		var htmlCode = "";
-		upHelper.result.reverse();
-		for(var i = 0 ; i < upHelper.result.length; i++) {
+		for(var i = 0 ; i < upHelper.ratingChange.length; i++) {
 
-			var newRow = insertProperty(response, "contest_id", upHelper.result[i]["contestId"]);
-			newRow = insertProperty(newRow, "contest_name", upHelper.result[i]["contestName"]);
+			var newRow = response;
+			newRow = insertProperty(newRow, "contest_id", upHelper.ratingChange[i]["contestId"]);
+			newRow = insertProperty(newRow, "contest_name", upHelper.ratingChange[i]["contestName"]);
 			htmlCode += newRow;
 		}
 		insertHTML("#main-content", htmlCode);
+
+		$ajaxUtil.sendGetRequest(problemsURL, loadProblems, true);
+
 	}
 
 	var buildHTML = function (response) {
 
 		if(response.status != "OK") return;
-		upHelper.result = response.result;
+		upHelper.ratingChange = response.result.reverse();
 
 		$ajaxUtil.sendGetRequest(contestListHTML, loadContestNames, false);
 	}
@@ -47,8 +91,8 @@
 
 		function handleData(event) {
 			var inputHandle = document.querySelector("#inputHandle").value;
-			var cfAPIUrl = "https://codeforces.com/api/user.rating?handle=" +  inputHandle;
-			$ajaxUtil.sendGetRequest(cfAPIUrl, buildHTML, true);
+			var newRatingChangeURL = ratingChangeURL +  inputHandle;
+			$ajaxUtil.sendGetRequest(newRatingChangeURL, buildHTML, true);
 		}
 		document.querySelector("button").addEventListener("click",handleData);
 	});
